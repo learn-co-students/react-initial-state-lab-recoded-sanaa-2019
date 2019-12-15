490f910d10583c9c21e06d60dc32a187526065f3
Firas Al-Azizy <firasazizy614@gmail.com>
2019-12-15 01:14:27 -0800

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,37 @@
-// your Bomb code here!
+
+import  React, { Component }  from 'react';
+
+
+
+class ImageSlider extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            secondsLeft:props.initialCount
+        }}
+
+
+render(){
+    return(
+        <div>
+            {
+                this.bomber()
+            }
+      </div>
+    )
+}
+
+
+
+bomber(){
+    if(this.state.secondsLeft>0){
+        return`${this.state.secondsLeft} seconds left before I go boom!`
+
+    }
+    else if (this.state.secondsLeft===0){
+        return'Boom!'
+    }
+}
+}
+
+export default ImageSlider;
